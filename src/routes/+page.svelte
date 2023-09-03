f5e3d47674218d1cb03ae9c58430c1600441b6f6
Fathy Osama <titooo01125@gmail.com>
2023-09-03 14:34:17 +0300

    Add : headers

---
+++ src/routes/+page.svelte
@@ -0,0 +1,72 @@
+<script>
+	import '@fontsource/poppins/700.css'
+	import Search from "./Search.svelte";
+	import Personal from './Personal.svelte';
+
+</script>
+
+<div class="header-layout">
+	<div class="py-[1rem]">
+		<div class="h-inline">
+			<p class="text-c-2-a">Chat with us</p>
+			<p class="text-c-1-a">+420 336 775 664</p>
+			<p class="text-c-1-a">info@freshnesecom.com</p>
+		</div>
+
+		<div class="h-inline">
+			<a href="/">Blog</a>
+			<a href="/">About Us</a>
+			<a href="/">Careers</a>
+		</div>
+	</div>
+	<div class="">
+		<!-- logo -->
+		<div class="h-[3.125rem] mr-1">
+			<div class="inline-flex py-[1rem]">
+				<div class="w-[11.0625rem] h-[1.125rem] bg-[url(./logo.svg)]"></div>
+			</div>
+		</div>
+		<Search />
+		<Personal />
+	</div>
+</div>
+
+<style lang="postcss">
+	.header-layout {
+		display: flex;
+		padding: 0rem 5rem;
+		flex-direction: column;
+		align-items: center;
+
+		background: var(--c-1-j, #fff);
+	}
+	.header-layout > div {
+		@apply flex h-[3rem] w-full justify-between;
+		max-width: 73.125rem;
+		/* flex-direction: column;
+		justify-content: flex-end;
+		align-items: flex-start; */
+	}
+	.header-layout > div > .h-inline {
+		display: inline-flex;
+		align-items: center;
+		gap: 2.0625rem;
+	}
+	.header-layout > div > .h-inline > p {
+		font-family: Open Sans;
+		font-size: 0.75rem;
+		font-style: normal;
+		font-weight: 400;
+		line-height: normal;
+	}
+	.header-layout > div > .h-inline > a {
+		color: var(--c-2-a, #6a983c);
+		font-family: Open Sans;
+		font-size: 0.75rem;
+		font-style: normal;
+		font-weight: 400;
+		line-height: normal;
+	}
+
+	/*  */
+</style>
---
+++ src/routes/Search.svelte
@@ -0,0 +1,80 @@
+<div class="h-[2.625rem] w-[31.25rem]">
+	<div class="flex h-full w-full rounded-[0.75rem] border border-c-1-d bg-c-1-h">
+		<button class="default">
+			<p>
+				<span class="mr-2">All categories</span>
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					width="10"
+					height="6"
+					viewBox="0 0 10 6"
+					fill="none"
+				>
+					<path
+						d="M1.81323 1.53333L4.55323 4.27332C4.67814 4.39749 4.84711 4.46719 5.02323 4.46719C5.19936 4.46719 5.36832 4.39749 5.49323 4.27332L8.1599 1.60666"
+						stroke="#6A983C"
+						stroke-width="2.5"
+						stroke-linecap="round"
+						stroke-linejoin="bevel"
+					/>
+				</svg>
+			</p>
+		</button>
+		<input class="grow" type="text" placeholder="Search Products, categories ..." />
+		<button class="search">
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				width="14"
+				height="14"
+				viewBox="0 0 14 14"
+				fill="none"
+			>
+				<path
+					d="M8.19401 10.4333C10.7713 10.4333 12.8607 8.34394 12.8607 5.76661C12.8607 3.18928 10.7713 1.09995 8.19401 1.09995C5.61668 1.09995 3.52734 3.18928 3.52734 5.76661C3.52734 8.34394 5.61668 10.4333 8.19401 10.4333Z"
+					stroke="#151515"
+					stroke-width="1.5"
+					stroke-linecap="round"
+					stroke-linejoin="bevel"
+				/>
+				<path
+					d="M4.81417 9.24002L1.6875 12.3667"
+					stroke="#151515"
+					stroke-width="1.5"
+					stroke-linecap="round"
+					stroke-linejoin="bevel"
+				/>
+			</svg>
+		</button>
+	</div>
+</div>
+
+<style lang="postcss">
+	button.default {
+		@apply flex h-full w-[10rem] flex-col justify-center pl-[1rem];
+		color: var(--c-1-a, #151515);
+		/* Button/Default */
+		font-family: Poppins;
+		font-size: 0.9375rem;
+		font-style: normal;
+		font-weight: 700;
+		line-height: normal;
+	}
+	button.default > p {
+		@apply flex w-full items-center border-r;
+	}
+	input {
+		@apply px-[1.5rem] placeholder:text-c-1-c;
+		font-family: Open Sans;
+		font-size: 0.875rem;
+		font-style: normal;
+		font-weight: 400;
+		line-height: normal;
+	}
+	.search {
+		@apply flex w-[2.75rem] h-full items-center justify-center;
+	}
+	.search svg {
+		width: 0.69831rem;
+		height: 0.70419rem;
+	}
+</style>
